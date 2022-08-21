2404b633521021a30baf879446d56dff2b29f025
hassandaru <hassandaru@gmail.com>
2022-08-21 17:16:42 +1000

    viewcontrollers setup and detailview can show label and textfield programmatically placed using visual scripting language

---
+++ MilestoneProject5/DetailViewController.swift
@@ -0,0 +1,73 @@
+//
+//  DetailViewController.swift
+//  MilestoneProject5
+//
+//  Created by Hassan Sohail Dar on 21/8/2022.
+//
+
+import UIKit
+
+class DetailViewController: UIViewController {
+    var titleOfItem = ""
+    var detailsOfItems = ""
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        title = titleOfItem
+        drawViewItemsAndSetupConstraint()
+    }
+    
+
+    
+    func drawViewItemsAndSetupConstraint() {
+        // Do any additional setup after loading the view.
+        let label1 = UILabel()
+        label1.translatesAutoresizingMaskIntoConstraints = false
+        label1.backgroundColor = UIColor.black
+        label1.textColor = UIColor.white
+        label1.text = titleOfItem
+        label1.sizeToFit()
+        
+        let details = UILabel()
+        details.translatesAutoresizingMaskIntoConstraints = false
+
+//        details.isEditable = false //we dont want it to be editable
+        details.textColor = UIColor.white
+        details.backgroundColor = UIColor.black
+        details.text = detailsOfItems
+        details.sizeToFit()
+
+        view.addSubview(label1)
+        view.addSubview(details)
+        
+        //setting up constraints
+        //setting up label 1 width / height / up and down i think
+        label1.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 20).isActive = true
+        label1.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: 20).isActive = true
+        label1.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor, constant: 20).isActive = true
+        
+        label1.heightAnchor.constraint(equalTo: view.safeAreaLayoutGuide.heightAnchor, multiplier: 0.2, constant: -10).isActive = true
+        //setting up textfield section
+        details.topAnchor.constraint(equalTo: label1.bottomAnchor, constant: 10).isActive = true
+        details.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: 20).isActive = true
+        details.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor, constant: 20).isActive = true
+//
+        details.heightAnchor.constraint(equalTo: view.safeAreaLayoutGuide.heightAnchor, multiplier: 0.4, constant: 10).isActive = true
+        
+        details.bottomAnchor.constraint(lessThanOrEqualToSystemSpacingBelow: view.safeAreaLayoutGuide.bottomAnchor, multiplier: 0.1 ).isActive = true
+        
+        
+    }
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
--- MilestoneProject5/ViewController.swift
+++ MilestoneProject5/ViewController.swift
@@ -7,13 +7,36 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UITableViewController {
+    var shoppingListItemName = [String]()
+    var shoppingListItemDetails = [String]()
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        shoppingListItemName = ["check" ]
+        shoppingListItemDetails = ["first Item Details"]
     }
 
 
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        shoppingListItemName.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "ShoppingItem", for: indexPath)
+        cell.textLabel?.text = shoppingListItemName[indexPath.row]
+        return cell
+        
+    }
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        if let vc = storyboard?.instantiateViewController(withIdentifier: "DetailView") as? DetailViewController {
+            vc.titleOfItem = shoppingListItemName[indexPath.row]
+            vc.detailsOfItems = shoppingListItemDetails[indexPath.row]
+            navigationController?.pushViewController(vc, animated: true)
+
+        }
+    }
 }
 
